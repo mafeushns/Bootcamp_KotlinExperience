a1bfe5f99d44959a2af49daae51b18433a5c2636
mafeushns <matheusnog001@gmail.com>
2022-11-21 15:53:47 -0300

    .

---
+++ Desafio_de_Projeto/README.md
@@ -0,0 +1,10 @@
+# Aprenda Kotlin Com Exemplos: Desafio de Projeto (Lab)
+
+Desafio de Projeto criado para avaliação do conteúdo técnico explorado no repositório [aprenda-Kotlin-com-exemplos](https://github.com/digitalinnovationone/aprenda-kotlin-com-exemplos). **Nesse contexto, iremos abstrair o seguinte domínio de aplicação:**
+
+**A [DIO](https://web.dio.me) possui `Formações` incríveis que têm como objetivo oferecer um conjunto de `Conteúdos Educacionais` voltados para uma stack tecnológica específica, preparando profissionais de TI para o mercado de trabalho. `Formações` possuem algumas características importantes, como `nome`, `nível` e seus respectivos `Conteúdos Educacionais`. Além disso, tais experiências educacionais têm um comportamento relevante ao nosso domínio, definido pela capacidade de `matricular` um ou mais `Alunos`.**
+
+
+```Kotlin
+TODO("Crie uma solução em Kotlin abstraindo esse domínio. O arquivo [desafio.kt] te ajudará 😉")
+```
---
+++ Desafio_de_Projeto/desafio.kt
@@ -0,0 +1,61 @@
+enum class Level { BASIC, INTERMEDIARY, ADVANCED }
+enum class Post { STUDENT, TEACHER }
+
+data class Users(val fNome: String, val lNome: String, val email: String, val post: Post)
+
+data class EducationalContent(val name: String, val duration: Int, val level: Level)
+
+data class Formation(val nome: String, val contents: List<EducationalContent>, val level: Level) {
+
+    val registered = mutableListOf<Users>()
+   
+    fun addUser(user: Users) {
+         registered.add(user)
+    }
+   
+    fun getAddUser(){
+        for(user in registered){
+            println("|Nome: ${user.fNome+" "+user.lNome}| - |Email: ${user.email}| - (${user.post})")
+        }
+    }
+   
+    fun printEducationalContent(){
+        for(cont in contents){
+            println("${cont.name} - ${cont.duration}min - (${cont.level})")
+        }
+    }
+}
+
+fun main() {
+ 	val moduleKotlinB = EducationalContent("Linguagem Kotlin", 120, Level.BASIC)
+   val moduleAndroidStudioB = EducationalContent("Fundamentos Android Studio", 120, Level.BASIC)
+  	val moduleKotlinI = EducationalContent("Linguagem Kotlin", 180, Level.INTERMEDIARY)
+  	val moduleAndroidStudioI = EducationalContent("Fundamentos Multiscreen", 180, Level.INTERMEDIARY)
+   
+	val contentList = mutableListOf<EducationalContent>()
+  		contentList.add(moduleKotlinB)
+  		contentList.add(moduleAndroidStudioB)
+    	contentList.add(moduleKotlinI)
+  		contentList.add(moduleAndroidStudioI)
+   
+    val luan = Users("Luan","Moraes", "luanmoraes@gmail.com", Post.STUDENT)
+    val camili = Users("Camili","Compagnoni", "camilicompagnoni@gmail.com", Post.STUDENT)
+    val venilton = Users("Venilto","Falvo", "veniltonfalvo@gmail.com", Post.TEACHER)
+   
+    val androidTraining = Formation("Formação Android Developer", contentList, Level.INTERMEDIARY)
+     
+    androidTraining.addUser(luan)
+    androidTraining.addUser(camili)
+    androidTraining.addUser(venilton)
+   
+  	println(" -----------------------------------------")
+    println("|${androidTraining.nome} - ${androidTraining.level}|")
+    println(" -----------------------------------------")
+   
+    println("Grade do curso")
+    println(androidTraining.printEducationalContent())
+    println("******************************************")
+   
+    println("Usuarios cadastrados no curso")
+    println(androidTraining.getAddUser())
+}
